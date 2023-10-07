9324d3ea0bb30401b205a494ee6d2a055ae7c9f9
ArtMGreen <artyom5145@gmail.com>
2023-10-07 22:49:23 +0300

    New outputs: decision vector + all the information about the last step

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def simplex_iteration(A_nb, A_b, C_nb, C_b, b, problem_type):
+def simplex_iteration(A_nb, A_b, C_nb, C_b, b, problem_type, basic_var_nums, non_basic_var_nums):
     """
 
     :param A_nb: matrix containing non-basic variables coefficients from constraints
@@ -17,7 +17,11 @@ def simplex_iteration(A_nb, A_b, C_nb, C_b, b, problem_type):
 
     :param problem_type:  0/1 = minimization/maximization
 
-    :return: optimal value of objective function z
+    :param basic_var_nums:  list of current basic variables
+
+    :param non_basic_var_nums:  list of current non-basic variables
+
+    :return: optimal solution for LPP
     """
     # find inverse of A_b
     A_b_inv = np.linalg.inv(A_b)
@@ -25,7 +29,7 @@ def simplex_iteration(A_nb, A_b, C_nb, C_b, b, problem_type):
     Xb = np.matmul(A_b_inv, b)
 
     # Also known as "Z_j - C_j = C_b_j * B_j_inverse * P_j - C_j" in ASA:
-    temp_matrix = np.subtract(np.matmul(np.matmul(C_b, A_b_inv), A_nb), C_nb)
+    temp_matrix = np.subtract(C_b @ A_b_inv @ A_nb, C_nb)
 
     if problem_type == 0:
         # search for entering variable if it is min problem
@@ -33,14 +37,14 @@ def simplex_iteration(A_nb, A_b, C_nb, C_b, b, problem_type):
 
         # if we can't improve objective function more => return its value
         if temp_matrix[enteringVar] < 0:
-            return np.matmul(C_b, Xb)
+            return C_b, Xb, basic_var_nums, non_basic_var_nums
     else:
         # search for entering variable if it is max problem
         enteringVar = np.argmin(temp_matrix)
 
         # if we can't improve objective function more => return its value
         if temp_matrix[enteringVar] > 0:
-            return np.matmul(C_b, Xb)
+            return C_b, Xb, basic_var_nums, non_basic_var_nums
 
     # update non-basic variables coefficients with respect to current pivot
     newA = np.matmul(A_b_inv, A_nb[:, enteringVar])
@@ -55,22 +59,37 @@ def simplex_iteration(A_nb, A_b, C_nb, C_b, b, problem_type):
     A_nb[:, enteringVar], A_b[:, exitingVar] = A_b[:, exitingVar].copy(), A_nb[:, enteringVar].copy()
     # do the same logic swap for objective function's vectors of coefficients
     C_b[exitingVar], C_nb[enteringVar] = C_nb[enteringVar], C_b[exitingVar]
+    # and save the new numbers of basic variables
+    basic_var_nums[exitingVar], non_basic_var_nums[enteringVar] =\
+        non_basic_var_nums[enteringVar], basic_var_nums[exitingVar]
 
     # run new iteration
-    return simplex_iteration(A_nb, A_b, C_nb, C_b, Xb, problem_type)
+    return simplex_iteration(A_nb, A_b, C_nb, C_b, Xb, problem_type, basic_var_nums, non_basic_var_nums)
 
 
 def simplex(C, A, b, problem_type):
     # n - number of variables
     # m - number of constraints
     m, n = A.shape
+    non_basic_var_nums = np.arange(1, n + 1)
+    basic_var_nums = np.arange(n + 1, n + m + 1)
+    decision_vector = np.zeros(n)
 
     # on the zero iteration basic variable coefficients are unit vectors:
     B = np.identity(m)
     # and zeros in the objective function
     Cb = np.zeros(m)
 
-    return simplex_iteration(A, B, C, Cb, b, problem_type)
+    C_b, Xb, basic_var_nums, non_basic_var_nums = simplex_iteration(A, B, C, Cb, b,
+                                                                    problem_type,
+                                                                    basic_var_nums,
+                                                                    non_basic_var_nums)
+
+    mask = np.argsort(np.hstack((basic_var_nums, non_basic_var_nums)))
+    Xb_extended = np.hstack((Xb, np.zeros(n)))
+    decision_vector = Xb_extended[mask][:n]
+
+    return C_b, Xb, basic_var_nums, non_basic_var_nums, decision_vector
 
 
 # Input from the user
@@ -103,7 +122,14 @@ while True:
 
     accuracy = float(input("Enter the approximation accuracy: "))
 
-    optimal = simplex(obj_function_coefficients_C, non_basic_coefficients_A, right_hand_side_vector_b, minmax-1)
-    print(optimal)
+    C_b, Xb, basic_var_nums, non_basic_var_nums, decision_vector = simplex(obj_function_coefficients_C,
+                                                                           non_basic_coefficients_A,
+                                                                           right_hand_side_vector_b, minmax-1)
+    print(f"Basic variables' coefficients: {C_b}")
+    print(f"Basic variable numbers: {basic_var_nums}")
+    print(f"Basic variable values: {Xb}")
+    print(f"Non-basic variable numbers: {non_basic_var_nums}")
+    print(f"Decision vector: {decision_vector}")
+    print(f"Objective function value: {C_b @ Xb}")
 
     break
